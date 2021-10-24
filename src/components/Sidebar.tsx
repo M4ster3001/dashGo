46ae9991319d5f7c8aea05f7a5048e19149201fe
M4ster3001-Ubuntu <aldocfjunior@gmail.com>
2021-10-24 09:05:49 -0300

    criado o componente de paginação, dashboard e criar usuário

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import Icon from '@chakra-ui/icon';
 import { Stack, Box, Text, Link } from '@chakra-ui/layout';
-import React from 'react';
 import {
   RiContactsLine,
   RiDashboardLine,
@@ -22,13 +22,13 @@ export function Sidebar() {
             Geral
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Link as={} to="" display="flex" align="center">
+            <Link to="" display="flex" align="center">
               <Icon as={RiDashboardLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Dashboard
               </Text>
             </Link>
-            <Link as={} to="" display="flex" align="center">
+            <Link to="" display="flex" align="center">
               <Icon as={RiContactsLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Usuários
@@ -41,13 +41,13 @@ export function Sidebar() {
             Automação
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Link as={} to="" display="flex" align="center">
+            <Link to="" display="flex" align="center">
               <Icon as={RiInputMethodLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Formulários
               </Text>
             </Link>
-            <Link as={} to="" display="flex" align="center">
+            <Link to="" display="flex" align="center">
               <Icon as={RiGitMergeLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Automação
